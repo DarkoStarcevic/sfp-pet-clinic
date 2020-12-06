bd272a5110f0552e974ea4f4f4dddc9c78a68c05
Darko Starcevic <67766140+DarkoStarcevic@users.noreply.github.com>
2020-12-06 23:07:47 +0100

    Update Map Implementation to manage setting of ID Property. Closes #23

--- pet-clinic-web/src/main/java/dare/springframework/sfppetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dare/springframework/sfppetclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Darko");
         owner1.setLastName("Starcevic");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Djordje");
         owner2.setLastName("Starcevic");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners ....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Mladen");
         vet1.setLastName("Lazic");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Masa");
         vet2.setLastName("Lazic");
 
