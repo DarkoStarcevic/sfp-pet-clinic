4c8de44b7a22a953242f772612c6a9067fd53d75
Darko Starcevic <67766140+DarkoStarcevic@users.noreply.github.com>
2020-12-09 02:06:47 +0100

    Create Vet Speciality Entity, Associate To Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/dare/springframework/sfppetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package dare.springframework.sfppetclinic.model;
+
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
