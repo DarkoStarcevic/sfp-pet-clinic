2f5d2343d9a885bebf1392e4a10cf7f325d5a1d1
Darko Starcevic <67766140+DarkoStarcevic@users.noreply.github.com>
2020-12-09 03:02:44 +0100

    Add PetTypes in with Bootstrap (startup). Closes #32

--- pet-clinic-web/src/main/java/dare/springframework/sfppetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dare/springframework/sfppetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package dare.springframework.sfppetclinic.bootstrap;
 
 import dare.springframework.sfppetclinic.model.Owner;
+import dare.springframework.sfppetclinic.model.PetType;
 import dare.springframework.sfppetclinic.model.Vet;
 import dare.springframework.sfppetclinic.services.OwnerService;
+import dare.springframework.sfppetclinic.services.PetTypeService;
 import dare.springframework.sfppetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,29 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setPetType("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+        // petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setPetType("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+       // petTypeService.save(cat);
+
+        System.out.println("Loaded pets ....");
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Darko");
