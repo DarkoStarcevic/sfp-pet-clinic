52e9e715e7c60ea104f1d17dbc6ff2e810b52d6d
Darko Starcevic <67766140+DarkoStarcevic@users.noreply.github.com>
2020-12-09 02:46:55 +0100

    Create Pet Type Map Service impl. Closes #29

---
+++ pet-clinic-data/src/main/java/dare/springframework/sfppetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package dare.springframework.sfppetclinic.services;
+
+import dare.springframework.sfppetclinic.model.PetType;
+
+public interface PetTypeService extends CrudServices<PetType, Long>{
+
+
+
+
+}
