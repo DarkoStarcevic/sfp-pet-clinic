6a3af3e293b398eb87a6775dceda7b47c4590946
Darko Starcevic <67766140+DarkoStarcevic@users.noreply.github.com>
2020-12-11 23:46:25 +0100

    Convert Owners to JPA Entities. Closes #36

--- pet-clinic-data/src/main/java/dare/springframework/sfppetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/dare/springframework/sfppetclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package dare.springframework.sfppetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "pet_type")
     private String petType;
 
     public String getPetType() {
