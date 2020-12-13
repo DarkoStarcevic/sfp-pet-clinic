e7a7c3f80d521afe9bb15450b75ccc26a29f468e
Darko Starcevic <67766140+DarkoStarcevic@users.noreply.github.com>
2020-12-13 03:34:36 +0100

    Create Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/dare/springframework/sfppetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package dare.springframework.sfppetclinic.services;
+
+import dare.springframework.sfppetclinic.model.Visit;
+
+public interface VisitService extends CrudServices<Visit, Long>{
+}
--- pet-clinic-data/src/main/java/dare/springframework/sfppetclinic/services/map/PetSMapService.java
+++ pet-clinic-data/src/main/java/dare/springframework/sfppetclinic/services/map/PetSMapService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
+public class PetSMapService extends AbstractMapService<Pet,Long> implements PetService {
 
 
     @Override
