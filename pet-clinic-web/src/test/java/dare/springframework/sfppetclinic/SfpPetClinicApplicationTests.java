8a258b37d1a9cf2e58fe185365aa16c92a4ac342
Darko Starcevic <67766140+DarkoStarcevic@users.noreply.github.com>
2020-12-25 00:26:28 +0100

    Upgrade to JUnit 5. Closes #11

--- pet-clinic-web/src/test/java/dare/springframework/sfppetclinic/SfpPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/dare/springframework/sfppetclinic/SfpPetClinicApplicationTests.java
@@ -1,13 +1,17 @@
 package dare.springframework.sfppetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfpPetClinicApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
